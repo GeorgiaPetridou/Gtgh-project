9bf5c6b367f660857baaacd8a5d4630f4eff1ecf
Dpapadopoulou <116271267+Dpapadopoulou@users.noreply.github.com>
2025-01-14 23:01:52 +0200

    interface

--- EventSystem/src/main/java/com/example/demo/services/ApprovalRequestService.java
+++ EventSystem/src/main/java/com/example/demo/services/ApprovalRequestService.java
@@ -1,11 +1,5 @@
 package com.example.demo.services;
 
-import java.util.List;
-
-import com.example.demo.users.ApprovalRequest;
-
-
 public interface ApprovalRequestService {
-	 List<ApprovalRequest> rejectRequest(Integer requestId,Integer employeeId, String comments);
-	 List<ApprovalRequest> approveRequest(Integer requestId, Integer employeeId, String comments);
+
 }
